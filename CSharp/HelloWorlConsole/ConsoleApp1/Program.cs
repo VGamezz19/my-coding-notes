80cdb8bf87bd6ca7da856426d434ec3d023d175f
VGamezz19 <victor.gamez1596@gmail.com>
2018-05-11 18:25:03 +0200

    HelloWorlConsole

---
+++ CSharp/HelloWorlConsole/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hola mundo");
+			Console.ReadLine();
+		}
+	}
+}
