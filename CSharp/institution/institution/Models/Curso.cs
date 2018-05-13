9985db5a9cc27398a44fc11e4edf912552f6f67e
VGamezz19 <victor.gamez1596@gmail.com>
2018-05-13 17:13:59 +0200

    struct and Class C#

--- CSharp/institution/institution/Models/Curso.cs
+++ CSharp/institution/institution/Models/Curso.cs
@@ -4,9 +4,16 @@ using System.Text;
 
 namespace institution.Models
 {
-    struct Curso
+    struct CursoStruct
     {
+		const string NOMBRE_DEFECTO_ASIGNADO = "NO ASIGNADA";
 		public string Curso { get; set; }
-		public short max_capacidad = 50;
+		public short max_capacidad;
+
+		public CursoStruct(short max_capa)
+		{
+			max_capacidad = max_capa;
+			Curso = NOMBRE_DEFECTO_ASIGNADO;
+		}
 	}
 }
