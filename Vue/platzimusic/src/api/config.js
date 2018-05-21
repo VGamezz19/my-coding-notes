645db97bafc1e703a031752aa74878a4d6c221fb
VGamezz19 <victor.gamez1596@gmail.com>
2018-05-21 22:10:55 +0200

    last.fm VUE

---
+++ Vue/platzimusic/src/api/config.js
@@ -0,0 +1,11 @@
+// Application name	platzimusic VUE
+// API key	f8f05e82def0e9005a2b1a519a105a73
+// Shared secret	e5d5545a0b315965a401a0171db604cf
+// Registered to	vgamez96
+
+export default {
+    appName: 'platzimusic',
+    apiKey:'8f05e82def0e9005a2b1a519a105a73',
+    secret:'e5d5545a0b315965a401a0171db604cf',
+    registeredTo:'vgamez96',
+}
--- Vue/platzimusic/src/components/HelloWorld.vue
+++ Vue/platzimusic/src/components/HelloWorld.vue
@@ -1,48 +1,23 @@
 <template lang="pug">
 .hello
-  h1 {{ msg }}
-  p
-    | For guide and recipes on how to configure / customize this project,
-    br
-    |       check out the
-    a(href='https://github.com/vuejs/vue-cli/tree/dev/docs', target='_blank') vue-cli documentation
-    | .
-  h3 Installed CLI Plugins
+  h1 PlatziMusic
   ul
-    li
-      a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel', target='_blank') babel
-    li
-      a(href='https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint', target='_blank') eslint
-  h3 Essential Links
-  ul
-    li
-      a(href='https://vuejs.org', target='_blank') Core Docs
-    li
-      a(href='https://forum.vuejs.org', target='_blank') Forum
-    li
-      a(href='https://chat.vuejs.org', target='_blank') Community Chat
-    li
-      a(href='https://twitter.com/vuejs', target='_blank') Twitter
-  h3 Ecosystem
-  ul
-    li
-      a(href='https://router.vuejs.org/en/essentials/getting-started.html', target='_blank') vue-router
-    li
-      a(href='https://vuex.vuejs.org/en/intro.html', target='_blank') vuex
-    li
-      a(href='https://github.com/vuejs/vue-devtools#vue-devtools', target='_blank') vue-devtools
-    li
-      a(href='https://vue-loader.vuejs.org', target='_blank') vue-loader
-    li
-      a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
-
+    li(v-for="artist in artists") {{ artist.name }}
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    artists: [
+      {name: 'David Bowie'},
+      {name: 'David Bowie'},
+      {name: 'David Bowie'},
+      {name: 'David Bowie'},
+      {name: 'David Bowie'},
+      {name: 'David Bowie'},
+      {name: 'David Bowie'}
+      ]
   }
 }
 </script>
