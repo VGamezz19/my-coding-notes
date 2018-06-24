7ae124845d8d69b935f7aa54c97c2f9e258f6350
VGamezz19 <victor.gamez1596@gmail.com>
2018-06-24 18:27:16 +0200

    v-for

--- Vue/platzi-music-profesional/src/App.vue
+++ Vue/platzi-music-profesional/src/App.vue
@@ -1,20 +1,17 @@
 <template lang="pug">
-- var essentialLinks =[{url:'https://vuejs.org', name: 'Core Docs'}, {url:'https://forum.vuejs.org', name: 'Forum'},{url:'https://chat.vuejs.org', name:'Community Chat'},{url:'https://twitter.com/vuejs', name:'TwitterChat'}]
-- var ecosystemLinks =[{url:'http://router.vuejs.org/', name: 'vue-router'},{url:'http://vuex.vuejs.org/', name: 'vuex'},{url:'http://vue-loader.vuejs.org/', name: 'vue-loader'},{url:'https://github.com/vuejs/awesome-vue', name: 'awesome-vue'}]
 
 #app
   img(src="./assets/logo.png")
-  h1 {{ msg }}
+  p {{ person.name }}
+  h1(v-show="showValue") {{ msg }}
   h2 Essential Links
     ul
-      each link in essentialLinks
-        li
-          a(href=link.url)= link.name
+        li(v-for="link in essentialLinks") 
+          a(href="link.url") {{link.name}}
   h2 Ecosystem
     ul
-      each link in ecosystemLinks
-        li
-          a(href=link.url)= link.name
+      li(v-for="link in ecosystemLinks") 
+        a(href="link.url") {{link.name}}
 </template> 
 
 <script>
@@ -22,17 +19,33 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      essentialLinks: [
+        {url: 'https://vuejs.org', name: 'Core Docs'},
+        {url: 'https://forum.vuejs.org', name: 'Forum'},
+        {url: 'https://chat.vuejs.org', name: 'Community Chat'},
+        {url: 'https://twitter.com/vuejs', name: 'TwitterChat'}
+      ],
+      ecosystemLinks: [
+        {url: 'http://router.vuejs.org/', name: 'vue-router'},
+        {url: 'http://vuex.vuejs.org/', name: 'vuex'},
+        {url: 'http://vue-loader.vuejs.org/', name: 'vue-loader'},
+        {url: 'https://github.com/vuejs/awesome-vue', name: 'awesome-vue'}
+      ],
+      person: {
+        name: 'Victor'
+      },
+      showValue: true
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import './scss/main.scss';
+@import "./scss/main.scss";
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -40,7 +53,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
