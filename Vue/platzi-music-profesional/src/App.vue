5bb8ad13b001d003e896640faefeb19bca1a2419
VGamezz19 <victor.gamez1596@gmail.com>
2018-06-24 17:47:11 +0200

    pug initial view

--- Vue/platzi-music-profesional/src/App.vue
+++ Vue/platzi-music-profesional/src/App.vue
@@ -1,22 +1,20 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+- var essentialLinks =[{url:'https://vuejs.org', name: 'Core Docs'}, {url:'https://forum.vuejs.org', name: 'Forum'},{url:'https://chat.vuejs.org', name:'Community Chat'},{url:'https://twitter.com/vuejs', name:'TwitterChat'}]
+- var ecosystemLinks =[{url:'http://router.vuejs.org/', name: 'vue-router'},{url:'http://vuex.vuejs.org/', name: 'vuex'},{url:'http://vue-loader.vuejs.org/', name: 'vue-loader'},{url:'https://github.com/vuejs/awesome-vue', name: 'awesome-vue'}]
+
+#app
+  img(src="./assets/logo.png")
+  h1 {{ msg }}
+  h2 Essential Links
+    ul
+      each link in essentialLinks
+        li
+          a(href=link.url)= link.name
+  h2 Ecosystem
+    ul
+      each link in ecosystemLinks
+        li
+          a(href=link.url)= link.name
 </template>
 
 <script>
