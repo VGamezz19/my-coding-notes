e49343c65fa0edbcc2c1e8394119ce7c64dad35a
VGamezz19 <victor.gamez1596@gmail.com>
2018-07-05 18:41:53 +0200

    init

---
+++ Vue/1-vue-tasks/src/App.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
+
+</template>
+
+<script>
+const tracks = [
+  {name: 'Muchacha', artist: 'Luis Albert Spinetta'},
+  {name: 'Hoy aca en el baile', artist: 'El Pepo'},
+  {name: 'I was made for loving you', artist: 'Kiss'}
+]
+export default {
+  name: 'app',
+  data () {
+    return {
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      console.log(this.searchQuery)
+      this.tracks = tracks
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./scss/main.scss";
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  // color: #2c3e50;
+  margin-top: 60px;
+}
+
+.results {
+  margin-top: 1rem;
+}
+</style>
