60b8d7f3b0e21d83a6f180e87dd3a752fdcb9d2e
VGamezz19 <victor.gamez1596@gmail.com>
2018-07-05 18:26:09 +0200

    integrate platzimusic

--- Vue/platzi-music-advanced/src/App.vue
+++ Vue/platzi-music-advanced/src/App.vue
@@ -1,85 +1,47 @@
 <template lang="pug">
+  #app
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
 
-#app
-  img(src="./assets/logo.png")
-  p {{ person.name }}
-  h1(v-show="showValue") {{ msg }}
-  h2 Essential Links
-    ul
-        li(v-for="link in essentialLinks") 
-          a(href="link.url") {{link.name}}
-  h2 Ecosystem
-    ul
-      li(v-for="link in ecosystemLinks") 
-        a(href="link.url") {{link.name}}
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
 
-  h3 Insert Name and last Name
-  input(v-model="name")
-  input(v-model="lastName")
-  button(v-on:click="format") Format
-  // button(@:click="format") Format
-
-  p {{ formattedName }}
-
-  p computed name {{ fullName }}
-
-  h3 Insert year
-
-  input(v-model="year")
-  p conputed year  {{ age }}
-  
-</template> 
+</template>
 
 <script>
+const tracks = [
+  {name: 'Muchacha', artist: 'Luis Albert Spinetta'},
+  {name: 'Hoy aca en el baile', artist: 'El Pepo'},
+  {name: 'I was made for loving you', artist: 'Kiss'}
+]
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      essentialLinks: [
-        {url: 'https://vuejs.org', name: 'Core Docs'},
-        {url: 'https://forum.vuejs.org', name: 'Forum'},
-        {url: 'https://chat.vuejs.org', name: 'Community Chat'},
-        {url: 'https://twitter.com/vuejs', name: 'TwitterChat'}
-      ],
-      ecosystemLinks: [
-        {url: 'http://router.vuejs.org/', name: 'vue-router'},
-        {url: 'http://vuex.vuejs.org/', name: 'vuex'},
-        {url: 'http://vue-loader.vuejs.org/', name: 'vue-loader'},
-        {url: 'https://github.com/vuejs/awesome-vue', name: 'awesome-vue'}
-      ],
-      person: {
-        name: 'Victor'
-      },
-      name: '',
-      lastName: '',
-      year: '',
-      formattedName: '',
-      showValue: true
+      searchQuery: '',
+      tracks: []
     }
   },
-  // Una transformacion de los datos, añadindole una funcionalidad de visualizacion.
   computed: {
-    fullName () {
-      return `${this.name} ${this.lastName}`
-    },
-
-    age () {
-      return new Date().getFullYear() - this.year
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
-  // No son propiedades. Se enlazan directamente con una variable del view model. asique SE HAN DE LLAMAR IGUAL
-  // Son funciones
-  watch: {
-    name (newVal, OldVal) {
-      console.log(newVal, OldVal)
-    }
-  },
-  // Para definir los eventos... pero no especificamentem un metodo esta enlazado a un Evento, tambien puede ser ejecutado dentro
-  // de un watcher o computed propertie.
   methods: {
-    format () {
-      this.formattedName = this.name.split() + this.lastName.split()
+    search () {
+      console.log(this.searchQuery)
+      this.tracks = tracks
     }
   }
 }
@@ -97,22 +59,7 @@ export default {
   margin-top: 60px;
 }
 
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.results {
+  margin-top: 1rem;
 }
 </style>
