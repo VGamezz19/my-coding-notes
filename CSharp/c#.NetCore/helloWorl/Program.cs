0182b8e2b11adb1f4d9b222cc3aa4e8e75260ac9
VGamezz19 <victor.gamez1596@gmail.com>
2018-11-01 22:50:28 +0100

    debugC#

---
+++ CSharp/c#.NetCore/helloWorl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace helloWorl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Program();
+
+            test.penis = "Penises";
+            
+            Console.WriteLine("Hello World!", test.penis);
+            
+        }
+
+        public string penis { get; set; }
+    }
+}
--- CSharp/c#.NetCore/helloWorl/obj/Debug/netcoreapp2.1/helloWorl.AssemblyInfo.cs
+++ CSharp/c#.NetCore/helloWorl/obj/Debug/netcoreapp2.1/helloWorl.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("helloWorl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyProductAttribute("helloWorl")]
+[assembly: System.Reflection.AssemblyTitleAttribute("helloWorl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
