2b004a7e8019ac38b34d8fad1494c94bfe4d1f27
VGamezz19 <victor.gamez1596@gmail.com>
2019-01-19 12:56:32 +0100

    filters vue

--- Vue/platzi-music-advanced/src/components/Player.vue
+++ Vue/platzi-music-advanced/src/components/Player.vue
@@ -4,7 +4,7 @@
       img(:src="track.album.images[0].url")
     p
       strong {{ track.name }}
-      small [{{ track.duration_ms }}]
+      small [{{ track.duration_ms | ms-to-mm }}]
     p
       audio(controls)
         source(:src="track.preview_url")
--- Vue/platzi-music-advanced/src/main.js
+++ Vue/platzi-music-advanced/src/main.js
@@ -3,12 +3,13 @@ import VueRouter from 'vue-router'
 import App from '@/App.vue'
 
 import routes from '@/routes'
-
+import msToMm from '@/filters/ms-to-mm'
 import { eventBusPlugin, objectPlugin } from '@/plugin'
 
 Vue.use(VueRouter)
 Vue.use(eventBusPlugin)
 Vue.use(objectPlugin)
+Vue.use(msToMm)
 
 const router = new VueRouter({
   routes,
