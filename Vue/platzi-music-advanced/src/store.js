5ba447a906c5bd830776751fd3355a7c28f1187f
VGamezz19 <victor.gamez1596@gmail.com>
2019-01-20 14:14:27 +0100

    platzi music vuex

---
+++ Vue/platzi-music-advanced/src/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import trackService from '@/services/track'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    track: {}
+  },
+
+  getters: {
+    trackTitle (state) {
+      if (!state.track.name) { return '' }
+      return `${state.track.name} - ${state.track.artists[0].name}`
+    }
+  },
+
+  mutations: {
+    setTrack (state, track) {
+      state.track = track
+    }
+  },
+
+  actions: {
+    getTrackById (context, payload) {
+      return trackService.getById(payload.id)
+        .then(res => {
+          context.commit('setTrack', res)
+          return res
+        })
+    }
+  }
+})
+
+export default store
