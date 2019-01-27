f8ff8038e628c41fb7cd1863edc681eee215daf4
VGamezz19 <victor.gamez1596@gmail.com>
2019-01-27 12:14:08 +0100

    node local modules

---
+++ NodeJs/01-node-local-modules/index.js
@@ -0,0 +1,18 @@
+/**
+ * *************
+ * Comience aqui
+ * *************
+ */
+
+const math = require("./math");
+const greet = require("./greetings");
+const hello = require("./greetings/hello");
+
+// Execute this code with F5
+console.log(math.add(4,5));
+console.log(math.divide(4,5));
+console.log(math.substract(4,5));
+console.log(math.multiply(4,5));
+
+console.log(greet.greet("Victor"));
+console.log(hello.sayHello("Victor Gamez"));
