9b4b5fc5771fdd5f96f19bf2a146229a58753a4e
VGamezz19 <victor.gamez1596@gmail.com>
2019-01-27 12:19:28 +0100

    subtract

--- NodeJs/01-node-local-modules/index.js
+++ NodeJs/01-node-local-modules/index.js
@@ -11,8 +11,8 @@ const hello = require("./greetings/hello");
 // Execute this code with F5
 console.log(math.add(4,5));
 console.log(math.divide(4,5));
-console.log(math.substract(4,5));
+console.log(math.subtract(4,5));
 console.log(math.multiply(4,5));
 
 console.log(greet.greet("Victor"));
-console.log(hello.sayHello("Victor Gamez"));
+console.log(hello.sayHello("Victor Gomez"));
--- NodeJs/01-node-local-modules/math.js
+++ NodeJs/01-node-local-modules/math.js
@@ -2,7 +2,7 @@ module.exports = {
     add: function(a,b) {
         return a + b;
     },
-    substract: function(a,b) {
+    subtract: function(a,b) {
         return a - b;
     },
     multiply: function(a,b) {
