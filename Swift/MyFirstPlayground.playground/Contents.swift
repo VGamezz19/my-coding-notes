14f36d128e2f0088c517324ec9af915c872af5a7
VGamezz19 <victor.gamez1596@gmail.com>
2019-07-27 16:46:03 +0200

    collections

--- Swift/MyFirstPlayground.playground/Contents.swift
+++ Swift/MyFirstPlayground.playground/Contents.swift
@@ -20,3 +20,36 @@ type(of: myResult)
 myResult = a / b
 type(of: myResult)
 
+var foo = "asd2"
+
+var soo = Int(foo) // nil = null
+
+foo = "12"
+
+soo = Int(foo) // 12
+
+
+// Optioanls, and unwrapped variables
+
+
+//1
+var optionalString: Int?
+
+optionalString = 100
+
+if optionalString != nil {
+    var unwraped = optionalString!
+    unwraped += 10
+    print(unwraped)
+}
+
+//2
+optionalString = 100
+
+// This is "optional binding"
+if var unwrappedOptional = optionalString {
+    unwrappedOptional += 100
+    print(unwrappedOptional)
+} else {
+    // Do something
+}
