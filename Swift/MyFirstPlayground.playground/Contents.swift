9d1eac62668905f03119b56d831bcfa883ed59ed
VGamezz19 <victor.gamez1596@gmail.com>
2019-07-28 18:39:40 +0200

    structurte

--- Swift/MyFirstPlayground.playground/Contents.swift
+++ Swift/MyFirstPlayground.playground/Contents.swift
@@ -212,3 +212,142 @@ func show(message: String, number: Int) -> Void {
 show(message: "Hola que tal, show message")
 show(number: 200)
 show(message: "showing", number: 200)
+
+
+//Enumeration
+newChapter(title: "enumeration")
+
+enum MediaType {
+    case book
+    case movie
+    case music
+    case game
+}
+
+var itemType: MediaType
+
+itemType = MediaType.book
+
+//later
+itemType = MediaType.movie
+
+// you can do that.
+itemType = .music
+
+// Raw Values
+// Especifict default value for each enum
+enum DefaultValue: String {
+    case hola = "Hola"
+    case que = "Que"
+    case tal = "Tal"
+}
+
+var defaultValueQuetal: DefaultValue = .hola
+
+switch defaultValueQuetal {
+case .hola, .que, .tal:
+    print("Default Vale in type \(defaultValueQuetal) there is as a default \(defaultValueQuetal.rawValue)")
+}
+
+// Associated Values in enumerables
+enum Object {
+    case movie(String)
+    case music(Int)
+    case book(String)
+}
+
+var items: [Object] = [.movie("Comedy"), .music(120), .book("Hobbit")]
+
+for item in items {
+    switch item {
+    case .movie(let genre):
+        print("The movie is of genre \(genre)")
+    case .music(let bpm):
+        print("the track is long \(bpm)s")
+    case .book(let title):
+        print("The best book ever is \(title)")
+    }
+}
+
+newChapter(title: "Structs")
+// Structs
+// Defining types, that contais other types.
+// IMPORTANT: You cannnot extend erencia
+
+struct Movie {
+    var title: String
+    var director: String
+    var releaseYear: Int
+    var genre: String
+    
+    func showInfo() -> String {
+        return "The movie \(title) released in \(releaseYear) by \(director)"
+    }
+}
+
+// But you need to inicialice
+var first = Movie(title: "AnalRape", director: "mi pene", releaseYear: 2019, genre: "Sex")
+var second = Movie(title: "Cicuta", director: "TioCicu", releaseYear: 2018, genre: "Muerte")
+
+first.releaseYear = 1889
+
+print(first.showInfo())
+print(second.showInfo())
+
+// dictonaries
+newChapter(title: "Dictonaries")
+
+//Key | Values
+var airlines = ["SWA": "Southwest Airlines",
+                "XUM": "North Blums",
+                "PGB": "German Paralx",
+                "BAW": "British Airways"]
+// var airlines:[String: String]
+
+print(airlines)
+var air = airlines["SWA"]
+print(air)
+
+// All the values in a dictionari are optional, because, could be that the key dosen't exist in the dictionary...
+
+if let a = airlines["SWA"] {
+    print(a)
+}
+
+// Add new
+airlines["CIC"] = "CICUTA"
+
+print(airlines)
+
+// Remove
+airlines["SWA"] = nil
+print(airlines)
+
+// loops
+// (key, value)
+for (code, airline) in airlines {
+    print(airline)
+}
+
+// Tuples
+newChapter(title: "Tuples")
+
+// The tuples are values wrapeds between parentesis.
+
+func randomAlbum() -> (albumTitle: String, albumDuration: Int) {
+    let title = "Lord of the rings"
+    let duration = 2546
+    
+    return (title, duration)
+}
+
+let rAlbum = randomAlbum()
+print(rAlbum)
+
+print(rAlbum.albumTitle)
+print(rAlbum.albumDuration)
+
+// like spread operator
+let (titAlbum, dudAlbum) = randomAlbum()
+
+print("\(titAlbum) \(dudAlbum)")
