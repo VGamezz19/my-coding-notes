550c098c4687464ac847a28323375dd1a517e727
VGamezz19 <victor.gamez1596@gmail.com>
2021-03-27 19:30:01 +0100

    import

---
+++ Deno/importExport/arithmetic.ts
@@ -0,0 +1,10 @@
+/**
+ * arithmetic.ts
+ */
+export function add(a: number, b: number): number {
+	return a + b;
+}
+
+export function multiply(a: number, b: number): number {
+	return a * b;
+}
---
+++ Deno/importExport/import.ts
@@ -0,0 +1,3 @@
+import { add, multiply } from './arithmetic.ts';
+
+console.log(multiply(2, 4));
